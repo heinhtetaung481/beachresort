618532dad9eabedb25c973fcf81140ee0540f1f3
ClassicalHHA <heinhtetaung481@gmail.com>
2020-11-22 00:57:35 +0800

    rooms page is done, fully functinal multiple filter is added

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,13 +1,19 @@
 import React from 'react'
-import RoomsFilter from './RoomFilter';
-import RoomsList from './RoomList';
+import RoomsFilter from './RoomFilter'
+import RoomsList from './RoomList'
+import {RoomConsumer, withRoomConsumer} from '../context'
+import Loading from './Loading'
 
-export default function RoomContainer() {
+const RoomContainer = ({context}) => {
+    const {loading, sortedRooms, rooms} = context;
+    if(loading) return <Loading/>;
+
     return (
         <div>
-            Hello from rooms container
-            <RoomsFilter/>
-            <RoomsList/>
+            <RoomsFilter rooms={rooms}/>
+            <RoomsList rooms={sortedRooms}/>
         </div>
     )
 }
+
+export default withRoomConsumer(RoomContainer);
